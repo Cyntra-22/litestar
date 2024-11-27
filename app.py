7db76e981eb080f733175c92e96426c75d598592
Cyntra <140875968+Cyntra-22@users.noreply.github.com>
2024-11-27 23:00:50 +0700

    litestar testing

---
+++ app.py
@@ -0,0 +1,13 @@
+from litestar import Litestar, get
+
+TODO_LIST: list[dict[str,str | bool]] = [
+    {"title": "Start writing TODO list", "done": True},
+    {"title": "???", "done": False},
+    {"title": "Profit", "done": False},
+]
+
+@get("/")
+async def get_list() -> list[dict[str,str|bool]]:
+    return TODO_LIST
+
+app = Litestar([get_list])
